6fa8949381732b9b04802116ff043249ca939e99
吴学锋 <wuxuefeng@techstar.com.cn>
2019-09-23 17:44:44 +0800

    创建环境

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,18 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <a-button type="primary" @click="handleClick">Primary</a-button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'app',
-  components: {
-    HelloWorld
+  methods:{
+    handleClick(){
+      console.log('111')
+  }
   }
 }
 </script>
